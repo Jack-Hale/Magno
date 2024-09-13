3fd0fa04362f82d1f6d45b211f6d362551521825
Jack-Hale <71580609+Jack-Hale@users.noreply.github.com>
2024-09-13 11:56:44 +0930

    [MAG-1] Player can now move and jump

--- Source/Player.cs
+++ Source/Player.cs
@@ -3,37 +3,73 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float MAX_SPEED = 300.0f;
+	public const float JUMP_VELOCITY = -400.0f;
+	public const float FRICTION = 400.0f;
+	public const float AIR_FRICTION = 1.0f;
+
+	public const float ACCELERATION = 400.0f;
+	public const float AIR_ACCELERATION = 1.0f;
+	Vector2 Input = Vector2.Zero;
+
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+	public override void _PhysicsProcess(double delta) {
+		Vector2 NewVelocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+			NewVelocity.Y += gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+		if (Godot.Input.IsActionJustPressed("Jump") && IsOnFloor())
+			NewVelocity.Y = JUMP_VELOCITY;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+		NewVelocity.X = MovePlayer(delta);
 
-		Velocity = velocity;
+		Velocity = NewVelocity;
 		MoveAndSlide();
+	}
+
+	public Vector2 GetInput() {
+		// Only X input is read because jump is handled separately
+		Vector2 InputX = Input;
+		InputX.X = Godot.Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown").X;
+		return InputX.Normalized();
+	}
+
+	public float MovePlayer(double delta) {
+		Input = GetInput();
+		Vector2 NewVelocity = Vector2.Zero;
+
+		// Needs to be only on X otherwise LimitLength takes falling and jumping into account affecting speed
+		NewVelocity.X = Velocity.X;
+		
+		// No Input
+		if (Input == Vector2.Zero)
+		{
+			// Player is moving, Apply friction to reduce speed
+			if (Math.Abs(NewVelocity.X) > (FRICTION * (float)delta))
+			{
+				// Friction is set based on land or air
+				NewVelocity -= NewVelocity.Normalized() * (IsOnFloor() ? FRICTION : AIR_FRICTION) * (float)delta;
+			}
+
+			// Player is not moving
+			else
+			{
+				NewVelocity = Vector2.Zero;
+			}
+		}
+		// Input, Add acceleration
+		else if (!Godot.Input.IsActionPressed("MoveDown") || IsOnFloor())
+		{
+			NewVelocity += Input * (IsOnFloor() ? ACCELERATION : AIR_ACCELERATION) * (float)delta;
+			NewVelocity = NewVelocity.LimitLength(MAX_SPEED);
+		}
+
+		return NewVelocity.X;
 	}
 }
