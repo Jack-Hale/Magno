dbb82031b27d4e266a439d61f897e1870c9827f2
Jack-Hale <71580609+Jack-Hale@users.noreply.github.com>
2024-09-17 14:51:13 +0930

    [MAG-2] Added second joint to the magnet to make the connection stronger.

--- Source/Magnet.cs
+++ Source/Magnet.cs
@@ -5,13 +5,15 @@ public partial class Magnet : Area2D
 {
 	private bool ActivationStatus = false;
 	private PhysicsBody2D EnteredBody;
-	private PinJoint2D _joint;
+	private PinJoint2D _joint1;
+	private PinJoint2D _joint2;
 	private float rotationFix = 0;
 	private bool canJoin = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		_joint = GetNode<PinJoint2D>("FixedJoint2D");
+		_joint1 = GetNode<PinJoint2D>("PinJoint2D_1");
+		_joint2 = GetNode<PinJoint2D>("PinJoint2D_2");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,11 +22,12 @@ public partial class Magnet : Area2D
 
 
 		if (EnteredBody != null && ActivationStatus && canJoin) {
-			_joint.NodeB = EnteredBody.GetPath();
+			_joint1.NodeB = EnteredBody.GetPath();
+			_joint2.NodeB = EnteredBody.GetPath();
 		}
 		if (!ActivationStatus) {
-			
-			_joint.NodeB = null;
+			_joint1.NodeB = null;
+			_joint2.NodeB = null;
 		}
 	}
 
