5a7b3590dfb29d551f3275da7a8fbde873fa77a7
Jack-Hale <71580609+Jack-Hale@users.noreply.github.com>
2025-02-28 13:47:06 +1030

    [MAG-7] health bar remains on enemy when in bodycopy mode

--- Source/Components/HealthComponent.cs
+++ Source/Components/HealthComponent.cs
@@ -13,7 +13,7 @@ public struct DamageCooldown {
     }
 }
 
-[Tool]
+// [Tool]
 public partial class HealthComponent : Node2D {
 	[Export]
 	float maxHealth = 100;
@@ -21,104 +21,132 @@ public partial class HealthComponent : Node2D {
 	float health;
 
 	ProgressBar _progressBar;
+	ProgressBar progressBarPassThrough;
 
 	CharacterBody2D character;
 	bool requirePassThrough = false;
 	HealthComponent passThroughHC;
+	bool sceneClass = false;
 
 	List<DamageCooldown> activeCooldowns = new List<DamageCooldown>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		if (GetChildCount() == 0) {
+			if (GetParent() is CharacterBody2D parent) {
+				PackedScene scene = (PackedScene)GD.Load("res://Scenes/Components/health_component.tscn");
+				Node healthComponent = scene.Instantiate();
+				Array<Node> children = healthComponent.GetChildren();
 
-		if (GetParent() is CharacterBody2D parent) {
+				for (int i = 0; i < children.Count; i++) {
+					// children[i].Owner = null;
+					// healthComponent.RemoveChild(children[i]);
+					AddChild(children[i].Duplicate());
+				}
 
-			PackedScene scene = (PackedScene)GD.Load("res://Scenes/Components/health_component.tscn");
-			Node healthComponent = scene.Instantiate();
-			Array<Node> children = healthComponent.GetChildren();
+				health = maxHealth;
+				_progressBar = GetNode<ProgressBar>("ProgressBar");
+				_progressBar.MaxValue = maxHealth;
+				_progressBar.Value = maxHealth;
 
-			for (int i = 0; i < children.Count; i++) {
-				children[i].Owner = null;
-				healthComponent.RemoveChild(children[i]);
-				AddChild(children[i]);
-			}
+				character = parent;
 
-			health = maxHealth;
-			_progressBar = GetNode<ProgressBar>("ProgressBar");
-			_progressBar.MaxValue = maxHealth;
-			_progressBar.Value = maxHealth;
 
-			character = parent;
+			} else {
+				if (GetParent().IsInGroup("BodyCopy")) {
+					requirePassThrough = true;
+					Array<Node> parentChildren = GetParent().GetParent().GetChildren();
+					for (int i = 0; i < parentChildren.Count; i++) {
+						if (parentChildren[i] is CharacterBody2D character) {
+							Array<Node> characterChildren = character.GetChildren();
 
-		} else {
-			if (GetParent().IsInGroup("BodyCopy")) {
-				requirePassThrough = true;
-				Array<Node> parentChildren = GetParent().GetParent().GetChildren();
-				for (int i = 0; i < parentChildren.Count; i++) {
-					if (parentChildren[i] is CharacterBody2D character) {
-						Array<Node> characterChildren = character.GetChildren();
-
-						for (int j = 0; j < characterChildren.Count; j++) {
-							if (characterChildren[j] is HealthComponent healthComponent) {
-								passThroughHC = healthComponent;
+							for (int j = 0; j < characterChildren.Count; j++) {
+								if (characterChildren[j] is HealthComponent healthComponent) {
+									passThroughHC = healthComponent;
+									_progressBar = (ProgressBar) healthComponent.GetProgressBar().Duplicate();
+									progressBarPassThrough = healthComponent.GetProgressBar();
+									AddChild(_progressBar);
+								}
 							}
 						}
 					}
-				}
 
-			} else {
-				GD.PrintErr($"HealthComponent {this}, does not have parent of type CharacterBody2D. Parent is, {GetParent().GetType()}, {GetParent().Name}");
-				GD.PushError($"HealthComponent {this}, does not have parent of type CharacterBody2D. Parent is, {GetParent().GetType()}, {GetParent().Name}");
+
+				} else {
+					GD.PrintErr($"HealthComponent {this}, does not have parent of type CharacterBody2D. Parent is, {GetParent().GetType()}, {GetParent().Name}");
+					GD.PushError($"HealthComponent {this}, does not have parent of type CharacterBody2D. Parent is, {GetParent().GetType()}, {GetParent().Name}");
+				}
 			}
+		} else {
+			sceneClass = true;
 		}
 	}
 
 	public override void _PhysicsProcess(double delta) {
-		UpdateCooldowns((float) delta);
+		if (!sceneClass) {
 
-		if (health <= 0 && !requirePassThrough) {
-			RunDeathSequence();
+			UpdateCooldowns((float) delta);
+
+			if (requirePassThrough) {
+				if (_progressBar.Value != progressBarPassThrough.Value) {
+					_progressBar.Value = progressBarPassThrough.Value;
+				}
+			}
+
+			if (health <= 0 && !requirePassThrough) {
+				RunDeathSequence();
+			}
 		}
 	}
 
 	public void TakeDamage(float amount, float cooldown, ulong source) {
-		if (requirePassThrough) {
-			passThroughHC.TakeDamage(amount, cooldown, source);
-		} else {
-			_progressBar.Value = health;
+		if (!sceneClass) {
+			if (requirePassThrough) {
+				passThroughHC.TakeDamage(amount, cooldown, source);
+			} else {
+				_progressBar.Value = health;
 
-			for (int i = 0; i < activeCooldowns.Count; i++) {
-				if (activeCooldowns[i].DamageSource == source) {
-					return;
+				for (int i = 0; i < activeCooldowns.Count; i++) {
+					if (activeCooldowns[i].DamageSource == source) {
+						return;
+					}
 				}
-			}
 
-			health -= amount;
+				health -= amount;
 
-			if (activeCooldowns.Count < 30) {
-				activeCooldowns.Add(new DamageCooldown(source, cooldown));
+				if (activeCooldowns.Count < 30) {
+					activeCooldowns.Add(new DamageCooldown(source, cooldown));
+				}
 			}
 		}
 	}
 
 	public void UpdateCooldowns(float delta) {
-		if (requirePassThrough) {
-			passThroughHC.UpdateCooldowns(delta);
-		} else {
-			for (int i = 0; i < activeCooldowns.Count; i++) {
-				activeCooldowns[i] = new DamageCooldown(activeCooldowns[i].DamageSource, activeCooldowns[i].Timer - delta);
-				if (activeCooldowns[i].Timer <= 0) {
-					activeCooldowns.RemoveAt(i);
+		if (!sceneClass) {
+			if (requirePassThrough) {
+				passThroughHC.UpdateCooldowns(delta);
+			} else {
+				for (int i = 0; i < activeCooldowns.Count; i++) {
+					activeCooldowns[i] = new DamageCooldown(activeCooldowns[i].DamageSource, activeCooldowns[i].Timer - delta);
+					if (activeCooldowns[i].Timer <= 0) {
+						activeCooldowns.RemoveAt(i);
+					}
 				}
-			}
-		}			
+			}	
+		}		
 	}
 
 	public void RunDeathSequence() {
-		if (requirePassThrough) {
-			passThroughHC.RunDeathSequence();
-		} else {
-			character.GlobalPosition = Vector2.Inf;
+		if (!sceneClass) {
+			if (requirePassThrough) {
+				passThroughHC.RunDeathSequence();
+			} else {
+				character.GlobalPosition = Vector2.Inf;
+			}
 		}
+	}
+
+	public ProgressBar GetProgressBar() {
+		return _progressBar;
 	}
 }
